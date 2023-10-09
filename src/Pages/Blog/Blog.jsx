b2a77ec800f638b73743d94d4ceadee7f18ed687
sabbirsiddique <sabbirsiddique6798@gmail.com>
2023-10-09 17:53:19 +0600

    Blog page content added

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,10 +1,52 @@
-
+/* eslint-disable react/no-unescaped-entities */
 const Blog = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <div className="h-full flex flex-col justify-center items-center w-[870px] mx-auto">
+        <h1 className="mt-10 mb-8 text-4xl font-extrabold">The Magic of Weddings: A Celebration of Love</h1>
+        <img src="https://i.ibb.co/mXRq6CM/Wedding-Anniversary-Songs-bridal-and-groom.jpg" className="mb-5 rounded-lg" />
+
+        <p className="text-left font-rubik">
+          Love is a beautiful journey, and a wedding is the ultimate celebration
+          of that journey. It's a day when two souls come together to promise a
+          lifetime of love, companionship, and adventure. Whether you're
+          planning your own wedding or simply dreaming of the day, weddings have
+          a unique way of capturing our hearts. <br /> <br />
+          <span className="font-bold text-xl">A Day of Dreams</span> <br /> <br /> From the moment
+          a couple says "Yes!" to the proposal, the excitement begins. Every
+          detail, from the venue to the flowers, is carefully considered to
+          create a day that reflects the couple's unique love story. It's a day
+          filled with anticipation and joy, a day that friends and family
+          eagerly look forward to.<br /><br /> 
+          <span className="font-bold text-xl">Love in the Air</span> <br /> <br /> Weddings are about more than
+          just the aesthetics; they're about the love that surrounds the couple.
+          It's in the teary-eyed smiles of parents, the laughter of friends, and
+          the heartfelt vows exchanged at the altar. Love is the thread that
+          weaves through every moment, making a wedding an emotional and
+          unforgettable experience. <br /><br /> 
+          <span className="font-bold text-xl">A Unity of Traditions</span> <br /><br /> Weddings are a
+          beautiful blend of traditions, both old and new. From the tossing of
+          the bouquet to the cutting of the cake, each ritual carries its own
+          significance. Couples often incorporate customs from their cultures or
+          create unique rituals that resonate with them personally, adding a
+          touch of their own story to the day. <br /><br />
+           <span className="font-bold text-xl">The Power of Community</span> <br /><br /> One of the
+          most heartwarming aspects of weddings is the coming together of a
+          community to celebrate the couple. Friends and family travel from near
+          and far to witness the union, creating a strong sense of support and
+          togetherness. <br /><br /> 
+          <span className="font-bold text-xl">A New Beginning</span> <br /><br />
+          As the wedding day draws to a close, it marks the beginning of a new
+          chapter. The couple embarks on a journey together, armed with the love
+          and memories created on their wedding day. <br /><br /> In the end, weddings are a
+          testament to the power of love, bringing people together and filling
+          our hearts with hope and happiness. Whether you're planning your own
+          wedding or attending one as a guest, these celebrations of love are a
+          reminder that there is always room for more love and joy in our lives. <br /><br />
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
